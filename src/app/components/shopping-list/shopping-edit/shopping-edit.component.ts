486d2a4c811f9460503fbbd85d5a2b84e529a126
Zhouhao-Rui <976667412@qq.com>
2022-05-21 13:00:44 +0100

    refactor: refactor shopping list CRUD code base with props

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { Ingredient } from "src/app/shared_model/ingredient.model";
 
 @Component({
   selector: "app-shopping-edit",
@@ -6,7 +15,31 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild("nameInput") input_name: ElementRef;
+  @ViewChild("amountInput") input_amount: ElementRef;
+  @Input() ingredients: Array<Ingredient>;
+  @Output() ingredientAddEmitter: EventEmitter<Ingredient> =
+    new EventEmitter<Ingredient>();
+  @Output() ingredientDeleteEmitter: EventEmitter<string> =
+    new EventEmitter<string>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  addIngredients() {
+    let ingredient = new Ingredient(
+      this.input_name.nativeElement.value,
+      parseFloat(this.input_amount.nativeElement.value)
+    );
+    this.ingredientAddEmitter.emit(ingredient);
+  }
+
+  deleteIngredients() {
+    this.ingredientDeleteEmitter.emit(this.input_name.nativeElement.value);
+  }
+
+  reset() {
+    this.input_name.nativeElement.value = "";
+    this.input_amount.nativeElement.value = "";
+  }
 }
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -7,12 +7,37 @@ import { Ingredient } from "src/app/shared_model/ingredient.model";
   styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Array<Ingredient> = [
-    new Ingredient("Apples", 5),
-    new Ingredient("Pepper", 3),
-    new Ingredient("Onion", 2)
-  ];
+  ingredients: Array<Ingredient> = [];
+  ingredients_names: string[] = [];
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAddIngredient(ingrident: Ingredient) {
+    // judge if the ingredient exist in list
+    if (!this.ingredients_names.includes(ingrident.name)) {
+      this.ingredients.push(ingrident);
+      this.ingredients_names.push(ingrident.name);
+    } else {
+      // update
+      this.ingredients = this.ingredients.map((value, index) => {
+        if (value.name === ingrident.name) {
+          value.amount += ingrident.amount;
+        }
+        return value;
+      });
+    }
+    console.log(this.ingredients);
+  }
+
+  onDeleteIngredient(name: string) {
+    this.ingredients = this.ingredients.filter((value, index) => {
+      return value.name != name;
+    });
+    this.ingredients_names = this.ingredients_names.filter((value, index) => {
+      return value != name;
+    });
+    console.log(this.ingredients);
+    console.log(this.ingredients_names);
+  }
 }
