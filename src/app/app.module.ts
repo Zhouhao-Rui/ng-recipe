9ee4357b24f309eac1c4df911edbb3c46fc8ce74
Zhouhao-Rui <976667412@qq.com>
2022-05-24 19:42:34 +0100

    feat: add new recipe editing form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { RecipesComponent } from "./components/recipes/recipes.component";
@@ -30,7 +31,7 @@ import { RecipeStartComponent } from "./components/recipes/recipe-start/recipe-s
     RecipeEditComponent,
     RecipeStartComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
--- src/app/components/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { FormArray, FormControl, FormGroup } from "@angular/forms";
 import { ActivatedRoute, Params } from "@angular/router";
+import { RecipeService } from "src/app/services/recipe.service";
 
 @Component({
   selector: "app-recipe-edit",
@@ -9,13 +11,66 @@ import { ActivatedRoute, Params } from "@angular/router";
 export class RecipeEditComponent implements OnInit {
   id: number;
   editModel: boolean = false;
-  constructor(private route: ActivatedRoute) {}
+  recipeForm: FormGroup;
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.id = +params["id"];
       this.editModel = params["id"] != null;
       console.log(this.editModel);
+      this.initForm();
     });
+  }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = "";
+    let recipeDescription = "";
+    let recipeIngredients = new FormArray([]);
+
+    if (this.editModel) {
+      const recipe = this.recipeService.getRecipeById(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe["ingredients"]) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  getControls() {
+    // transform from AbstractControl[] to FormArray[]
+    return (<FormArray>this.recipeForm.get("ingredients")).controls;
+  }
+
+  addIngredient() {
+    (<FormArray>this.recipeForm.get("ingredients")).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl()
+      })
+    );
   }
 }
