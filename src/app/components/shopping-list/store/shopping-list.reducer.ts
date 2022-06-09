2571b398927f28978ad94a52257295f99caf43b7
Zhouhao-Rui <976667412@qq.com>
2022-06-09 21:26:04 +0100

    feat: add multiple ingredient action in NgRX

--- src/app/components/shopping-list/store/shopping-list.reducer.ts
+++ src/app/components/shopping-list/store/shopping-list.reducer.ts
@@ -1,16 +1,33 @@
 import { Ingredient } from "src/app/shared_model/ingredient.model";
-import { ADD_INGREDIENT } from "./shopping-list.action";
+import {
+  AddIngredient,
+  AddIngredients,
+  ADD_INGREDIENT,
+  ADD_INGREDIENTS,
+  ShoppingListActions
+} from "./shopping-list.action";
 
 const initialState = {
   ingredients: [new Ingredient("Apple", 5), new Ingredient("Pear", 10)]
 };
 
-export function shoppingListReducer(state = initialState, action) {
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions
+) {
   switch (action.type) {
     case ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action.payload]
+        ingredients: [...state.ingredients, (action as AddIngredient).payload]
+      };
+    case ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [
+          ...state.ingredients,
+          ...(action as AddIngredients).payload
+        ]
       };
     default:
       return state;
